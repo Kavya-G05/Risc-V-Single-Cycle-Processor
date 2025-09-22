5e6ee41f3be992605306dd36e6dba2f76e46748e
Kavya Ganesan <kavyaganesan05@gmail.com>
2025-09-22 17:11:00 +0530

    Create Mux2.v

---
+++ code/Mux2.v
@@ -0,0 +1,9 @@
+//Mux2 Adder_mux
+module Mux2(
+    input sel2,
+    input [31:0] A2,
+    input [31:0] B2,
+    output [31:0] Mux2_out
+    );
+    assign Mux2_out = (sel2==1'b0)? A2 : B2;
+endmodule
