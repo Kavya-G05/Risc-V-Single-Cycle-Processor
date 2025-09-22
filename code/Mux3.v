87a5ca14b273cbe3660dbb56582a585be0f98b1b
Kavya Ganesan <kavyaganesan05@gmail.com>
2025-09-22 17:11:26 +0530

    Create Mux3.v

---
+++ code/Mux3.v
@@ -0,0 +1,9 @@
+//Mux3 Memory_mux
+module Mux3(
+    input sel3,
+    input [31:0] A3,
+    input [31:0] B3,
+    output [31:0] Mux3_out
+    );
+    assign Mux3_out = (sel3==1'b0)? A3 : B3;
+endmodule
